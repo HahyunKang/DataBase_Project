c8b2ea5db87f66d9a1c2db1d16f23ce5ce6c9348
HahyunKang <ganghahyeon06@gmail.com>
2023-11-29 21:57:27 +0900

    커뮤니티 게시판 추가

---
+++ data/community.py
@@ -0,0 +1,41 @@
+import psycopg2
+import csv
+
+from SQLCreate import SQLCreate
+from connect import connect
+
+def community():
+    try:
+        conn = connect()
+        cursor = conn.cursor()
+        cursor.execute("DROP TABLE IF EXISTS Community;")
+        cursor.execute(
+            """CREATE TABLE Community (communityID Int, userId Int, communityName VARCHAR ); """
+        )
+
+        cursor.execute("""
+            INSERT INTO Community (communityId, userId, communityName)
+            SELECT r.regionId, u.userId, r.regionName || '게시판'
+            FROM Users u
+            JOIN region r ON u.region = r.regionName;
+        """)
+        cursor.execute("select * from Community;")
+        res = cursor.fetchall()
+        for data in res:
+            print(data)
+        conn.commit()
+
+
+
+    except psycopg2.Error as e:
+        print("Connection failure.")
+        raise e
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    community()
